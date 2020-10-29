cbe044b784e77a050eff4aaf20212924e7f147cd
quangkhanh27 <lamquankhanh2710@gmail.com>
2020-10-29 11:39:45 +0700

    khanh1

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .extensions import mongo
-from .Routers.TodoRouters import todo
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-app.config.from_object('app.settings')
-app.register_blueprint(todo)
